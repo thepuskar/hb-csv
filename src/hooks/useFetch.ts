a1dd37410985d6f09bdb7e7d62e8bbdb13ef989d
Puskar Adhikari <puskar.ad@gmail.com>
2022-10-14 22:31:39 +0545

    fetch hooks enable controlled and updated according to query key

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,12 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import { getApi } from 'utils';
 
-export const useFetch = (uniqueKey: string, URL: string) => {
+
+
+export const useFetch = (
+  uniqueKey: string,
+  URL: string,
+  enabled: boolean = false,
+  queryKey: string = ''
+) => {
   const queryFnc = async () => {
     return await getApi(URL);
   };
-  const { error, isLoading, data, refetch, isError, isSuccess } = useQuery([uniqueKey], queryFnc, {
-    retry: false,
-  });
+  const { error, isLoading, data, refetch, isError, isSuccess } = useQuery(
+    [uniqueKey, queryKey],
+    queryFnc,
+    {
+      retry: false,
+      enabled,
+    }
+  );
   return { error, isLoading, data, refetch, isError, isSuccess };
 };
