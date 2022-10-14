12b0ce0591ffbf4b7b0775cfa225883ce440595a
Puskar Adhikari <puskar.ad@gmail.com>
2022-10-14 22:32:26 +0545

    list skeleton component created

---
+++ src/components/skeleton/ListSkeleton.tsx
@@ -0,0 +1,18 @@
+import { generateRandNumArray } from 'utils';
+
+interface IProps {
+  count: number;
+}
+
+export const ListSkeleton = ({ count = 4 }: IProps) => {
+  return (
+    <div className="py-2 px-4">
+      {generateRandNumArray(count)?.map((count) => (
+        <p
+          key={count}
+          className="mb-3 leading-relaxed rounded-md py-2 px-4 w-full h-3 animate-pulse bg-gray-400"
+        ></p>
+      ))}
+    </div>
+  );
+};
