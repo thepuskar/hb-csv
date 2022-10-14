6f222d92317f1cbc56a8611070096fbff3291a74
Puskar Adhikari <puskar.ad@gmail.com>
2022-10-14 17:29:28 +0545

    added svg icon

---
+++ src/assets/index.ts
@@ -0,0 +1,2 @@
+export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as ChevronDown } from './ChevronDown.svg';
--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -2,6 +2,8 @@ import { useState, useRef } from 'react';
 import { useOnClickOutside } from 'usehooks-ts';
 import { useFetch } from 'hooks';
 
+import { SearchIcon, ChevronDown } from 'assets';
+
 interface ICategory {
   id: string;
   name: string;
@@ -38,20 +40,7 @@ export function SearchBar() {
             className="w-full flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200  dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white dark:border-gray-600"
             type="button"
           >
-            All categories{' '}
-            <svg
-              aria-hidden="true"
-              className="ml-1 w-4 h-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            All categories <ChevronDown className="ml-1 w-4 h-4" />
           </button>
           {showDropDown && (
             <div
@@ -102,21 +91,7 @@ export function SearchBar() {
             type="submit"
             className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800  dark:bg-blue-600 dark:hover:bg-blue-700"
           >
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
-            </svg>
+            <SearchIcon className="w-5 h-5" />
             <span className="sr-only">Search</span>
           </button>
         </div>
