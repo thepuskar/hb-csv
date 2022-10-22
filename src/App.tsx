12abd5a5596eb187b2d47586d18fc01700bfa637
Puskar Adhikari <puskar.ad@gmail.com>
2022-10-22 21:03:57 +0545

    sheet downloaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import { QueryClientProvider } from '@tanstack/react-query'
-// import { CSVLink, CSVDownload } from 'react-csv'
 import { queryClient } from 'config'
 import { ProductList, Layout, ProductDetail } from 'components'
 
 function App() {
-  // const headers = [
-  //   { label: 'First Name', key: 'firstname' },
-  //   { label: 'Last Name', key: 'lastname' },
-  //   { label: 'Email', key: 'email' },
-  // ]
-
-  // const data = [
-  //   { firstname: 'Ahmed', lastname: 'Tomi', email: 'ah@smthing.co.com' },
-  //   { firstname: 'Raed', lastname: 'Labes', email: 'rl@smthing.co.com' },
-  //   { firstname: 'Yezzi', lastname: 'Min l3b', email: 'ymin@cocococo.com' },
-  // ]
-
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App px-3  py-3">
         <Layout>
           <ProductList />
         </Layout>
-        {/* <CSVLink data={data} headers={headers}>
-          Download me
-        </CSVLink> */}
       </div>
     </QueryClientProvider>
   )
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { SearchBar } from 'components'
+import { SearchBar, DownloadSheet } from 'components'
 
 export const Navbar = () => {
   return (
@@ -15,12 +15,7 @@ export const Navbar = () => {
           </span>
         </a>
         <div className="flex md:order-2">
-          <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Get started
-          </button>
+          <DownloadSheet />
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
